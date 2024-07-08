3160d529d901c3bb799a93a449431539d07fa815
Srijit Bera <167683807+srijitbera@users.noreply.github.com>
2024-07-08 21:13:20 +0530

    Demo Python Code Calculation Program

---
+++ demo.py
@@ -0,0 +1,11 @@
+a = 12
+b = 30
+add = a + b
+sub = a - b
+mul = a * b
+div = a // b
+
+print("Addition result is:",add)
+print("Subtraction result is:",sub)
+print("Multiplication result is:",mul)
+print("Division result is:",div)
